1de28c70838a985d1505e1aac8586553e2b1bac6
Alexandre Akira <alexandreakiraakao@gmail.com>
2026-05-23 19:34:00 -0300

    fix(shortcuts): change default hotkey from ⌥⌘Space (Finder collision) to ⌃⌥Space

--- App/DevTrayApp/Shortcuts.swift
+++ App/DevTrayApp/Shortcuts.swift
@@ -1,8 +1,10 @@
 import KeyboardShortcuts
 
 extension KeyboardShortcuts.Name {
+    // ⌥⌘Space collides with Finder's "Show Finder Search Window" on macOS.
+    // ⌃⌥Space is unbound by default and ergonomic.
     static let toggleSpotlight = Self(
         "toggleSpotlight",
-        default: .init(.space, modifiers: [.option, .command])
+        default: .init(.space, modifiers: [.control, .option])
     )
 }
